Added input gradient and fixed num_params calculation in ansatz

src/model.py
@@ -93,7 +93,17 @@ class QuanvCircuit:
         return np.real(result)
 
     def grad_input(self, input_data, weights):
-        pass
+                        
+        expectation = StateFn(self.hamiltonian, is_measurement=True) @ StateFn(self.qc)
+        expectation = expectation.bind_parameters(dict(zip(self.weight_vars, weights)))
+                
+        grad = self.shifter.convert(expectation)
+        gradient_in_pauli_basis = PauliExpectation().convert(grad)
+        value_dict = dict(zip(self.input_vars, input_data))
+        
+        result = np.array(self.sampler.convert(gradient_in_pauli_basis, params=value_dict).eval())
+    
+        return np.real(result)
 
 
 class QuanvFunction(Function):